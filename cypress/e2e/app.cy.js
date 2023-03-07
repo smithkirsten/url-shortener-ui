fe07b5af408fcbffc376ac803156460e52438289
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-03-07 12:19:27 -0500

    Add beforeEach

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -1,6 +1,10 @@
 describe('Landing page of App', () => {
+  beforeEach(() => {
+    cy.intercept('GET', 'http://localhost:3001/api/v1/urls', { fixture: 'urls.json'})
+    cy.visit('http://localhost:3000/')
+  })
   it('Displays the title', () => {
-   
+    cy.get('h1').should('contain', 'URL Shortener')
   })
 
   it('Displays all urls', () => {
