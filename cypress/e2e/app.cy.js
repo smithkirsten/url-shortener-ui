10c7ebb488e2609dbf0a49a8baf26bc091b09e2e
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-03-07 12:05:48 -0500

    Add test file and shells for tests

---
+++ cypress/e2e/app.cy.js
@@ -0,0 +1,17 @@
+describe('Landing page of App', () => {
+  it('Displays the title', () => {
+   
+  })
+
+  it('Displays all urls', () => {
+   
+  })
+
+  it('Displays the input form', () => {
+   
+  })
+
+  it('Should display new post after it is submitted', () => {
+   
+  })
+})
