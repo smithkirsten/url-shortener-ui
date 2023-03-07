deda71a2f2265d0cdfb54003994c098664561aea
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-03-07 11:38:19 -0500

    Add post function

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,12 @@ export const getUrls = () => {
   return fetch('http://localhost:3001/api/v1/urls')
       .then(response => response.json())
 }
+
+export const postURL = (inputs) => {
+  return fetch('http://localhost:3001/api/v1/urls', {
+    method: 'POST',
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({...inputs, id: Date.now() })
+  })
+    .then(response => response.json())
+}
